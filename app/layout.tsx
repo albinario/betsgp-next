2fc4ff8af676189ca685142dbbff46c22dc68a93
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-12-14 09:06:20 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { AuthContextProvider } from '@/context/AuthContext'
+import './globals.css'
+import type { Metadata } from 'next'
+import Header from '@/components/common/Header'
+
+export const metadata: Metadata = {
+	title: 'Bet SGP',
+	description: 'define Bet SGP',
+	authors: [{ name: 'Albin Lindeborg', url: 'albinlindeborg.surge.sh' }]
+}
+
+export default function RootLayout({
+	children
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang='en'>
+			<body>
+				<AuthContextProvider>
+					<Header />
+					{children}
+				</AuthContextProvider>
+			</body>
+		</html>
+	)
+}
