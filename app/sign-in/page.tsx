d2d86292d916b5d7bb72fe84f2e4b14a754d1b80
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-12-14 16:23:48 +0100

    sign functionality

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,10 @@
 import AnimationWrapper from '@/components/AnimationWrapper'
+import SignInForm from '@/components/SignInForm'
 
 export default function SignIn() {
 	return (
 		<AnimationWrapper>
-			<></>
+			<SignInForm />
 		</AnimationWrapper>
 	)
 }
