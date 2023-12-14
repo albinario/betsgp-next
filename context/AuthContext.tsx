89928f86fcc4b2e417095e85f7b91719b201123b
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-12-14 09:49:47 +0100

    setup firebase auth with context

---
+++ context/AuthContext.tsx
@@ -0,0 +1,61 @@
+'use client'
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signOut,
+	User
+} from 'firebase/auth'
+import { auth } from '../firebase/config'
+import React, { createContext, useEffect, useState } from 'react'
+
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+type AuthContextType = {
+	user: User | null
+	signInUser: (email: string, password: string) => void
+	signOutUser: () => void
+	signUpUser: (email: string, password: string) => void
+}
+
+export const AuthContextProvider = ({
+	children
+}: {
+	children: React.ReactNode
+}) => {
+	const [user, setUser] = useState<User | null>(null)
+	const [loading, setLoading] = useState<Boolean>(true)
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUser(user)
+			} else {
+				setUser(null)
+			}
+		})
+
+		setLoading(false)
+
+		return unsubscribe
+	}, [])
+
+	const signInUser = (email: string, password: string) => {
+		return signInWithEmailAndPassword(auth, email, password)
+	}
+
+	const signOutUser = async () => {
+		setUser(null)
+		return await signOut(auth)
+	}
+
+	const signUpUser = (email: string, password: string) => {
+		return createUserWithEmailAndPassword(auth, email, password)
+	}
+
+	return (
+		<AuthContext.Provider value={{ user, signUpUser, signInUser, signOutUser }}>
+			{loading ? null : children}
+		</AuthContext.Provider>
+	)
+}
---
+++ types/Auth.types.ts
@@ -0,0 +1,9 @@
+export type SignIn = {
+	email: string
+	password: string
+}
+
+export type SignUp = {
+	email: string
+	password: string
+}
