1c57fc7423b99bc8fba7866f6d35e39fcec9088d
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-12-28 09:24:26 +0100

    reset prisma and start fresh

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,12 +4,12 @@ let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
 	prisma = new PrismaClient({
-		log: ['warn', 'error']
+		log: ['warn', 'error', 'query', 'info']
 	})
 } else {
 	if (!global.prisma) {
 		global.prisma = new PrismaClient({
-			log: ['warn', 'error']
+			log: ['warn', 'error', 'query', 'info']
 		})
 	}
 	prisma = global.prisma
