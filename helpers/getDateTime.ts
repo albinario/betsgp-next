a26b60c60d864eeb04718b9fca0e673bddccb2ef
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-08 22:39:56 +0100

    add date format alternative

--- helpers/getDateTime.ts
+++ helpers/getDateTime.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
 
-export default function getLocalDateTime(date: Date) {
-	return DateTime.fromJSDate(date).toLocal().toFormat('yyyy-MM-dd HH:mm')
+export default function getLocalDateTime(date: Date, short = false) {
+	return DateTime.fromJSDate(date)
+		.toLocal()
+		.toFormat(short ? 'dd/MM HH:mm' : 'yyyy-MM-dd HH:mm')
 }
