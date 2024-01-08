79ce0ff386b65d369425af872df248a666e933f0
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-08 22:39:32 +0100

    ability to add gp

---
+++ app/admin/AddGP.tsx
@@ -0,0 +1,106 @@
+'use client'
+import classNames from 'classnames'
+import type { city } from '@prisma/client'
+import { createGP } from '@/prisma/service'
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import CardBody from 'react-bootstrap/CardBody'
+import CardHeader from 'react-bootstrap/CardHeader'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import FormSelect from 'react-bootstrap/FormSelect'
+import Row from 'react-bootstrap/Row'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import type { GPNew } from '@/types'
+
+export default function AddGP({ cities }: { cities: city[] }) {
+	const [isSubmitting, setIsSubmitting] = useState(false)
+
+	const {
+		handleSubmit,
+		register,
+		formState: { errors }
+	} = useForm<GPNew>()
+
+	const onSubmit: SubmitHandler<GPNew> = async (data: GPNew) => {
+		try {
+			setIsSubmitting(true)
+
+			const gpNew: GPNew = {
+				cityId: Number(data.cityId),
+				dateTime: new Date(data.dateTime + 'Z'),
+				gp: Number(data.gp)
+			}
+
+			await createGP(gpNew)
+
+			toast.success('GP added')
+		} catch (error) {
+			toast.error('Something went wrong when trying to add GP')
+		} finally {
+			setIsSubmitting(false)
+		}
+	}
+
+	return (
+		<Col>
+			<Card>
+				<CardHeader>GP</CardHeader>
+				<CardBody className='p-2'>
+					<Form className='d-grid gap-2' onSubmit={handleSubmit(onSubmit)}>
+						<FormSelect
+							className={classNames({
+								'missing-border': errors.cityId
+							})}
+							size='sm'
+							{...register('cityId', { required: true })}
+						>
+							<option value=''>Select city</option>
+							{cities
+								?.sort((a, b) => a.name.localeCompare(b.name))
+								.map((city) => (
+									<option key={city.id} value={city.id}>
+										{city.name}
+									</option>
+								))}
+						</FormSelect>
+						<Row className='g-2'>
+							<Col>
+								<FormControl
+									className={classNames({
+										'missing-border': errors.gp
+									})}
+									placeholder='#'
+									type='number'
+									size='sm'
+									{...register('gp', { required: true })}
+								/>
+							</Col>
+							<Col xs={9}>
+								<FormControl
+									className={classNames({
+										'missing-border': errors.dateTime
+									})}
+									type='datetime-local'
+									size='sm'
+									{...register('dateTime', { required: true })}
+								/>
+							</Col>
+						</Row>
+						<Button
+							disabled={isSubmitting}
+							size='sm'
+							type='submit'
+							variant='outline-success'
+						>
+							+
+						</Button>
+					</Form>
+				</CardBody>
+			</Card>
+		</Col>
+	)
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,18 @@
 import AddCity from './AddCity'
+import AddGP from './AddGP'
 import AddNation from './AddNation'
 import AnimationWrapper from '@/components/AnimationWrapper'
-import { getNations } from '@/prisma/service'
+import { getCities, getNations } from '@/prisma/service'
 import Row from 'react-bootstrap/Row'
 
 export default async function Admin() {
+	const cities = await getCities()
 	const nations = await getNations()
 
 	return (
 		<AnimationWrapper>
-			<Row xs={1} sm={2} md={3} lg={4} xl={5} xxl={6} className='g-2'>
+			<Row xs={1} sm={2} md={3} lg={4} xl={5} className='g-2'>
+				<AddGP cities={cities} />
 				<AddCity nations={nations} />
 				<AddNation />
 			</Row>
