dcf041c84f63251ecd610c87c3e91165f34c9122
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-11 14:13:59 +0100

    shos session user outside top ten

--- components/user/Results.tsx
+++ components/user/Results.tsx
@@ -1,14 +1,15 @@
 import classNames from 'classnames'
 import Flag from '@/components/Flag'
-import { FlagCheckered, Medal } from '@/icons'
+import Medals from '@/components/Medals'
+import { FlagCheckered } from '@/icons'
 import Link from 'next/link'
 import { getUserResults } from '@/prisma/service'
+import type { ReactElement } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardHeader from 'react-bootstrap/CardHeader'
 import Table from 'react-bootstrap/Table'
 import type { GP, Rider } from '@/types'
-import Medals from '../Medals'
 
 export default async function UserResults({
 	gp,
@@ -22,132 +23,195 @@ export default async function UserResults({
 	userPicks: number[] | null
 }) {
 	let userResults = await getUserResults(gp.id)
+	if (!userResults) return <></>
 
-	if (topTen) userResults = userResults.slice(0, 10)
+	let userTr: ReactElement = <></>
 
-	let prev: number | null = 0
-	let showPos = true
+	if (userId && topTen) {
+		const userResult = userResults.filter(
+			(userResult) => userResult.userId === userId
+		)[0]
 
-	const pickedRiders: number[] = []
+		if (userResult) {
+			const picks: Rider[] = [
+				userResult.user.userPicks[0].pick1,
+				userResult.user.userPicks[0].pick2,
+				userResult.user.userPicks[0].pick3
+			]
+			userTr = (
+				<tr>
+					<td className='text-end'>{userResult.pos}</td>
+					<td className='pe-0 text-start'>
+						<Link className='highlight' href={'/users/' + userResult.userId}>
+							{userResult.user.firstName} {userResult.user.lastName}
+						</Link>
+					</td>
 
-	return (
-		<Card>
-			<CardHeader className='py-1 d-flex justify-content-center'>
-				{topTen && (
-					<div className='d-flex align-items-center'>
-						<Flag height='.8em' nationCode={gp.city.nation.code} />
-						<span className='ms-1'>{gp.city.name}</span>
-					</div>
-				)}
-				{!topTen && 'Results'}
-			</CardHeader>
-			<Table
-				borderless
-				className='mb-1 text-center'
-				hover
-				responsive
-				size='sm'
-				striped
-			>
-				<thead>
-					<tr>
-						<th colSpan={6} />
-						<th>
-							P<span className='d-none d-sm-inline'>oin</span>ts
-						</th>
-						<th>
-							<FlagCheckered />
-						</th>
-					</tr>
-				</thead>
-
-				<tbody>
-					{userResults.map((userResult) => {
-						showPos = userResult.pos !== prev
-						prev = userResult.pos
-
-						const picks: Rider[] = [
-							userResult.user.userPicks[0].pick1,
-							userResult.user.userPicks[0].pick2,
-							userResult.user.userPicks[0].pick3
-						]
-
-						return (
-							<tr key={userResult.id}>
-								<td className='text-end'>
-									<span className={classNames({ 'opacity-0': !showPos })}>
-										{userResult.pos}
-									</span>
-								</td>
-								<td className='pe-0 text-start'>
-									<Link
-										className={classNames({
-											highlight: userResult.userId === userId
-										})}
-										href={'/users/' + userResult.userId}
-									>
-										{userResult.user.firstName} {userResult.user.lastName}
-									</Link>
-								</td>
-
-								{picks
-									.sort((a, b) => a.number - b.number)
-									.map((pick) => {
-										const unique = !pickedRiders.includes(pick.id)
-										if (unique) pickedRiders.push(pick.id)
-
-										return (
-											<td
-												key={pick.id}
-												className='px-0 text-nowrap'
-												title={pick.name}
+					{picks
+						.sort((a, b) => a.number - b.number)
+						.map((pick) => (
+							<td key={pick.id} className='px-0 text-nowrap' title={pick.name}>
+								<Link href={'/riders/' + pick.id}>
+									<span style={{ paddingBottom: '2px' }}>
+										<Flag height='.7em' nationCode={pick.nation.code} />
+										<span
+											style={{
+												fontSize: '.8em',
+												marginLeft: '2px'
+											}}
+										>
+											<span
+												className={classNames({
+													highlight: userPicks?.includes(pick.id)
+												})}
 											>
-												<Link href={'/riders/' + pick.id}>
-													<span
-														className={classNames({ unique })}
-														style={{ paddingBottom: '2px' }}
-													>
-														<Flag height='.7em' nationCode={pick.nation.code} />
+												{pick.number}
+											</span>
+										</span>
+									</span>
+								</Link>
+							</td>
+						))}
+
+					<td className='text-nowrap'>
+						<Medals medals={[userResult.m1, userResult.m2, userResult.m3]} />
+					</td>
+					<td>{userResult.points}</td>
+					<td>{userResult.races}</td>
+				</tr>
+			)
+		}
+
+		if (topTen) userResults = userResults.slice(0, 10)
+
+		let prev: number | null = 0
+		let showPos = true
+
+		const pickedRiders: number[] = []
+
+		return (
+			<Card>
+				<CardHeader className='py-1 d-flex justify-content-center'>
+					{topTen && (
+						<div className='d-flex align-items-center'>
+							<Flag height='.8em' nationCode={gp.city.nation.code} />
+							<span className='ms-1'>{gp.city.name}</span>
+						</div>
+					)}
+					{!topTen && 'Results'}
+				</CardHeader>
+				<Table
+					borderless
+					className='mb-1 text-center'
+					hover
+					responsive
+					size='sm'
+					striped
+				>
+					<thead>
+						<tr>
+							<th colSpan={6} />
+							<th>
+								P<span className='d-none d-sm-inline'>oin</span>ts
+							</th>
+							<th>
+								<FlagCheckered />
+							</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{userResults.map((userResult) => {
+							showPos = userResult.pos !== prev
+							prev = userResult.pos
+
+							const picks: Rider[] = [
+								userResult.user.userPicks[0].pick1,
+								userResult.user.userPicks[0].pick2,
+								userResult.user.userPicks[0].pick3
+							]
+
+							return (
+								<tr key={userResult.id}>
+									<td className='text-end'>
+										<span className={classNames({ 'opacity-0': !showPos })}>
+											{userResult.pos}
+										</span>
+									</td>
+									<td className='pe-0 text-start'>
+										<Link
+											className={classNames({
+												highlight: userResult.userId === userId
+											})}
+											href={'/users/' + userResult.userId}
+										>
+											{userResult.user.firstName} {userResult.user.lastName}
+										</Link>
+									</td>
+
+									{picks
+										.sort((a, b) => a.number - b.number)
+										.map((pick) => {
+											const unique = !pickedRiders.includes(pick.id)
+											if (unique) pickedRiders.push(pick.id)
+
+											return (
+												<td
+													key={pick.id}
+													className='px-0 text-nowrap'
+													title={pick.name}
+												>
+													<Link href={'/riders/' + pick.id}>
 														<span
-															style={{
-																fontSize: '.8em',
-																marginLeft: '2px'
-															}}
+															className={classNames({ unique })}
+															style={{ paddingBottom: '2px' }}
 														>
+															<Flag
+																height='.7em'
+																nationCode={pick.nation.code}
+															/>
 															<span
-																className={classNames({
-																	highlight: userPicks?.includes(pick.id)
-																})}
+																style={{
+																	fontSize: '.8em',
+																	marginLeft: '2px'
+																}}
 															>
-																{pick.number}
+																<span
+																	className={classNames({
+																		highlight: userPicks?.includes(pick.id)
+																	})}
+																>
+																	{pick.number}
+																</span>
 															</span>
 														</span>
-													</span>
-												</Link>
-											</td>
-										)
-									})}
+													</Link>
+												</td>
+											)
+										})}
 
-								<td className='text-nowrap'>
-									<Medals
-										medals={[userResult.m1, userResult.m2, userResult.m3]}
-									/>
-								</td>
-								<td>{userResult.points}</td>
-								<td>{userResult.races}</td>
-							</tr>
-						)
-					})}
-				</tbody>
-			</Table>
+									<td className='text-nowrap'>
+										<Medals
+											medals={[userResult.m1, userResult.m2, userResult.m3]}
+										/>
+									</td>
+									<td>{userResult.points}</td>
+									<td>{userResult.races}</td>
+								</tr>
+							)
+						})}
+						{userTr}
+					</tbody>
+				</Table>
 
-			{topTen && (
-				<Link href={'/gps/' + gp.id} className='d-grid p-2'>
-					<Button size='sm' variant='outline-success'>
-						More
-					</Button>
-				</Link>
-			)}
-		</Card>
-	)
+				{topTen && (
+					<Link href={'/gps/' + gp.id} className='d-grid p-2'>
+						<Button size='sm' variant='outline-success'>
+							More
+						</Button>
+					</Link>
+				)}
+			</Card>
+		)
+	}
 }
--- components/user/Standings.tsx
+++ components/user/Standings.tsx
@@ -6,85 +6,134 @@ import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardHeader from 'react-bootstrap/CardHeader'
 import Table from 'react-bootstrap/Table'
+import type { ReactElement } from 'react'
 
-export default async function UserStandings({ topTen }: { topTen?: boolean }) {
+export default async function UserStandings({
+	topTen,
+	userId
+}: {
+	topTen?: boolean
+	userId?: number
+}) {
 	let userStandings = await getUserStandings(2023)
+	if (!userStandings) return <></>
 
-	if (topTen) userStandings = userStandings.slice(0, 10)
+	let userTr: ReactElement = <></>
 
-	let prev: number | null = 0
-	let showPos = true
+	if (userId && topTen) {
+		const userStanding = userStandings.filter(
+			(userStanding) => userStanding.userId === userId
+		)[0]
 
-	return (
-		<Card>
-			<CardHeader className='py-1 text-center'>Total standings</CardHeader>
-			<Table
-				borderless
-				className='mb-1 text-center'
-				hover
-				responsive
-				size='sm'
-				striped
-			>
-				<thead>
-					<tr>
-						<th colSpan={2} />
-						<th>Points</th>
-						{[1, 2, 3].map((m) => (
-							<th key={m}>
-								<Medal type={m} />
-							</th>
-						))}
-						<th>
-							<FlagCheckered />
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{userStandings.map((userStanding) => {
-						showPos = userStanding.pos === prev ? false : true
-						prev = userStanding.pos
-
-						return (
-							<tr key={userStanding.id}>
-								<td className='d-flex align-items-center justify-content-end'>
-									<span className={classNames({ 'opacity-0': !showPos })}>
-										{userStanding.pos}
-									</span>
-									{userStanding.pos && userStanding.prevPos && (
-										<Arrow diff={userStanding.prevPos - userStanding.pos} />
-									)}
-								</td>
-								<td className='text-start'>
-									<Link href={'/users/' + userStanding.userId}>
-										{userStanding.user.firstName} {userStanding.user.lastName}
-									</Link>
-								</td>
-								<td>{userStanding.points}</td>
-								{[userStanding.m1, userStanding.m2, userStanding.m3].map(
-									(medals, index) => (
-										<td
-											key={index}
-											className={classNames({ 'text-muted': !medals })}
-										>
-											{medals}
-										</td>
-									)
-								)}
-								<td>{userStanding.races}</td>
-							</tr>
+		if (userStanding) {
+			userTr = (
+				<tr>
+					<td className='d-flex align-items-center justify-content-end'>
+						{userStanding.pos}
+						{userStanding.pos && userStanding.prevPos && (
+							<Arrow diff={userStanding.prevPos - userStanding.pos} />
+						)}
+					</td>
+					<td className='text-start'>
+						<Link className='highlight' href={'/users/' + userStanding.userId}>
+							{userStanding.user.firstName} {userStanding.user.lastName}
+						</Link>
+					</td>
+					<td>{userStanding.points}</td>
+					{[userStanding.m1, userStanding.m2, userStanding.m3].map(
+						(medals, index) => (
+							<td key={index} className={classNames({ 'text-muted': !medals })}>
+								{medals}
+							</td>
 						)
-					})}
-				</tbody>
-			</Table>
+					)}
+					<td>{userStanding.races}</td>
+				</tr>
+			)
+		}
 
-			{topTen && (
-				<Link href={'/standings'} className='d-grid p-2'>
-					<Button size='sm' variant='outline-success'>
-						More
-					</Button>
-				</Link>
-			)}
-		</Card>
-	)
+		if (topTen) userStandings = userStandings.slice(0, 10)
+
+		let prev: number | null = 0
+		let showPos = true
+
+		return (
+			<Card>
+				<CardHeader className='py-1 text-center'>Total standings</CardHeader>
+				<Table
+					borderless
+					className='mb-1 text-center'
+					hover
+					responsive
+					size='sm'
+					striped
+				>
+					<thead>
+						<tr>
+							<th colSpan={2} />
+							<th>Points</th>
+							{[1, 2, 3].map((m) => (
+								<th key={m}>
+									<Medal type={m} />
+								</th>
+							))}
+							<th>
+								<FlagCheckered />
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{userStandings.map((userStanding) => {
+							showPos = userStanding.pos === prev ? false : true
+							prev = userStanding.pos
+
+							return (
+								<tr key={userStanding.id}>
+									<td className='d-flex align-items-center justify-content-end'>
+										<span className={classNames({ 'opacity-0': !showPos })}>
+											{userStanding.pos}
+										</span>
+										{userStanding.pos && userStanding.prevPos && (
+											<Arrow diff={userStanding.prevPos - userStanding.pos} />
+										)}
+									</td>
+									<td className='text-start'>
+										<Link
+											className={classNames({
+												highlight: userStanding.userId === userId
+											})}
+											href={'/users/' + userStanding.userId}
+										>
+											{userStanding.user.firstName} {userStanding.user.lastName}
+										</Link>
+									</td>
+									<td>{userStanding.points}</td>
+									{[userStanding.m1, userStanding.m2, userStanding.m3].map(
+										(medals, index) => (
+											<td
+												key={index}
+												className={classNames({ 'text-muted': !medals })}
+											>
+												{medals}
+											</td>
+										)
+									)}
+									<td>{userStanding.races}</td>
+								</tr>
+							)
+						})}
+						{userTr}
+					</tbody>
+				</Table>
+
+				{topTen && (
+					<Link href={'/standings'} className='d-grid p-2'>
+						<Button size='sm' variant='outline-success'>
+							More
+						</Button>
+					</Link>
+				)}
+			</Card>
+		)
+	}
 }
