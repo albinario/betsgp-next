ea3384daad0fa951f0b5f1fc3819ebd2f08df611
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-11 17:03:31 +0100

    hide prisma query logs

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,12 +4,13 @@ let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
 	prisma = new PrismaClient({
-		log: ['warn', 'error', 'query', 'info']
+		// log: ['warn', 'error', 'query', 'info']
+		log: ['warn', 'error', 'info']
 	})
 } else {
 	if (!global.prisma) {
 		global.prisma = new PrismaClient({
-			log: ['warn', 'error', 'query', 'info']
+			log: ['warn', 'error', 'info']
 		})
 	}
 	prisma = global.prisma
