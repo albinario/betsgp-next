bb218d2e5f8e7cd20a040cbbeafd593a667dcc4e
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-12 01:06:15 +0100

    create copyright placeholder

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,9 @@
+import getCurrentYear from '@/helpers/getCurrentYear'
+
 export default function Footer() {
-	return <></>
+	return (
+		<div className='d-flex justify-content-end mt-2 py-2 small text-muted'>
+			Copyright 2020-{getCurrentYear()}
+		</div>
+	)
 }
