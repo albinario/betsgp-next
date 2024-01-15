d25ad3a0f654bbd7405be7806b59bd955fe941a4
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-15 14:31:52 +0100

    add more button

--- components/gp/Upcoming.tsx
+++ components/gp/Upcoming.tsx
@@ -5,6 +5,7 @@ import { getDateTimeLocal } from '@/helpers/dateTime'
 import { Pick } from '@/icons'
 import Link from 'next/link'
 import { getGpsUpcoming, getRidersActive } from '@/prisma/service'
+import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardBody from 'react-bootstrap/CardBody'
 import Col from 'react-bootstrap/Col'
@@ -45,34 +46,42 @@ export default async function GPsUpcoming({
 						)}
 
 						{!userId && (
-							<Table
-								borderless
-								className='mb-1'
-								hover
-								responsive
-								size='sm'
-								striped
-							>
-								<tbody>
-									{gp.activity.map((act) => (
-										<tr key={act.id}>
-											<td className='pe-0 text-center'>
-												<Pick creation={act.creation} />
-											</td>
-											<td>
-												<Link href={'/users/' + act.userId}>
-													{act.user.firstName} {act.user.lastName}
-												</Link>
-											</td>
-											<td className='pe-2 text-end text-muted'>
-												<span className='small'>
-													{getDateTimeLocal(act.dateTime)}
-												</span>
-											</td>
-										</tr>
-									))}
-								</tbody>
-							</Table>
+							<>
+								<Table
+									borderless
+									className='mb-1'
+									hover
+									responsive
+									size='sm'
+									striped
+								>
+									<tbody>
+										{gp.activity.map((act) => (
+											<tr key={act.id}>
+												<td className='pe-0 text-center'>
+													<Pick creation={act.creation} />
+												</td>
+												<td>
+													<Link href={'/users/' + act.userId}>
+														{act.user.firstName} {act.user.lastName}
+													</Link>
+												</td>
+												<td className='pe-2 text-end text-muted'>
+													<span className='small'>
+														{getDateTimeLocal(act.dateTime)}
+													</span>
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</Table>
+
+								<Link href={'/gps/' + gp.id} className='d-grid p-2'>
+									<Button size='sm' variant='outline-success'>
+										More
+									</Button>
+								</Link>
+							</>
 						)}
 					</Card>
 				</Col>
