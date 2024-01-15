a4c5bcc3fb4572e248f1f8e51d36b5950be4a8ef
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-15 16:15:00 +0100

    remove type

--- components/Flag.tsx
+++ components/Flag.tsx
@@ -1,13 +1,15 @@
 import Flags from 'country-flag-icons/react/3x2'
 
-type Flag = {
+export default function Flag({
+	height,
+	nationCode
+}: {
 	height: string
 	nationCode?: string
-}
-
-export default function Flag({ height, nationCode }: Flag) {
+}) {
 	const FlagComponent = nationCode
 		? Flags[nationCode.toUpperCase() as keyof typeof Flags]
 		: null
+
 	return FlagComponent ? <FlagComponent style={{ height }} /> : null
 }
