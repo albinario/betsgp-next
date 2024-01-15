ce84d38d80f37195a5b7bef0ebc5a29be2fb6553
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-15 16:15:35 +0100

    improve check

--- helpers/array.ts
+++ helpers/array.ts
@@ -2,6 +2,8 @@ export const isIdentical = (
 	arr1: (number | null)[],
 	arr2: (number | null)[]
 ) => {
+	if (arr1.length !== arr2.length) return false
+
 	for (let i = 0; i < arr1.length; i++) {
 		if (arr1[i] !== arr2[i]) return false
 	}
