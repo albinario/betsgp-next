4b83e3edcafd825923f99de4ccd7b602251f91a9
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-15 23:54:01 +0100

    add copyright

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,10 @@
 import { getCurrentYear } from '@/helpers/dateTime'
+import { Copyright } from '@/icons'
 
 export default function Footer() {
 	return (
-		<div className='d-flex justify-content-end mt-2 py-2 small text-muted'>
-			Copyright 2020-{getCurrentYear()}
+		<div className='d-flex align-items-center justify-content-end gap-1 mt-2 py-2 small text-muted'>
+			A Lindeborg <Copyright /> 2020-{getCurrentYear()}
 		</div>
 	)
 }
