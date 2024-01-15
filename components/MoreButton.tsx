450ef371d623403060f03376b3c11b630563c4f2
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-01-15 16:46:34 +0100

    change color

--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -3,8 +3,8 @@ import Button from 'react-bootstrap/Button'
 
 export default function MoreButton({ href }: { href: string }) {
 	return (
-		<Link href={href} className='d-grid p-2'>
-			<Button size='sm' variant='outline-light'>
+		<Link href={href} className='d-grid opacity-50 p-2'>
+			<Button size='sm' variant='outline-warning'>
 				More
 			</Button>
 		</Link>
